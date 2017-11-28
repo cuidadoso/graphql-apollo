1e7209b6b9f55aac3139abd430702c2d7690dec0
cuidadoso <alexander_s@front.ru>
2017-11-28 15:07:41 +0300

    Add authentication token to Apollo

--- hackernews-react-apollo/src/index.js
+++ hackernews-react-apollo/src/index.js
@@ -6,15 +6,29 @@ import './styles/index.css';
 import {App} from './components';
 import registerServiceWorker from './registerServiceWorker';
 
-import {ApolloProvider} from 'react-apollo';
-import {ApolloClient} from 'apollo-client';
-import {HttpLink} from 'apollo-link-http';
-import {InMemoryCache} from 'apollo-cache-inmemory';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { ApolloLink } from 'apollo-client-preset';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import {GC_AUTH_TOKEN} from './constants';
 
 const httpLink = new HttpLink({ uri: 'https://api.graph.cool/simple/v1/cja86fefj2u2401443r75y0vu' });
 
+const middlewareAuthLink = new ApolloLink((operation, forward) => {
+  const token = localStorage.getItem(GC_AUTH_TOKEN);
+  const authorizationHeader = token ? `Bearer ${token}` : null;
+  operation.setContext({
+    headers: {
+      authorization: authorizationHeader
+    }
+  });
+  return forward(operation);
+});
+const httpLinkWithAuthToken = middlewareAuthLink.concat(httpLink);
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: httpLinkWithAuthToken,
   cache: new InMemoryCache()
 });
 
